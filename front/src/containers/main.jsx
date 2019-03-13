21a05cdb961800154d8fa4a3ba1d5d61dc8308a1
Juan Manuel Ibañez <jmiban@gmail.com>
2019-03-13 16:34:06 -0300

    header presentacional

--- front/src/containers/main.jsx
+++ front/src/containers/main.jsx
@@ -1,59 +1,52 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import { setSearch, setBookFund } from '../store/actions/actions'
-import Axios from 'axios';
-
-
+import React from "react";
+import { connect } from "react-redux";
+import { setSearch, setBookFund } from "../store/actions/actions";
+import Axios from "axios";
+import SearchBar from "../components/SearchBar.jsx";
+import Header from "../components/Header.jsx";
 
 class Main extends React.Component {
-
-
     handleChange(e) {
-        this.props.setSearch(e.target.value)
+      this.props.setSearch(e.target.value);
     }
-
+  
     componentDidUpdate(prevState) {
-        if (this.state.search == prevState.search)
-        Axios.get(`http://www.omdbapi.com/?apikey=8c8bfbdc&s=${this.props.search}`)
-            .then(books => {
-                console.log(books)
-                this.props.setBookFound(books)
-            }
-
-            )
+      if (this.state.search == prevState.search)
+        Axios.get(
+          `http://www.omdbapi.com/?apikey=8c8bfbdc&s=${this.props.search}`
+        ).then(books => {
+          console.log(books);
+          this.props.setBookFound(books);
+        });
     }
-
-
+  
     render() {
-        return (
-            <div>
-                <h1>Hola Titanes de nuevo!!!</h1>
-
-                <input onChange={this.handleChange.bind(this)} placeholder='buscame' />
-                <p>{this.props.search}</p>
-
-            </div>
-        )
+      return (
+        <div>
+          <section>
+            <Header />
+            </section>
+            <SearchBar />
+        </div>
+      );
     }
-
-
-
-}
-
-
-function mapStateToProps(state) {
+  }
+  
+  function mapStateToProps(state) {
     return {
-        search: state.search,
-        find: state.find
-    }
-}
-
-function mapDispatchToProps(dispatch) {
+      search: state.search,
+      find: state.find
+    };
+  }
+  
+  function mapDispatchToProps(dispatch) {
     return {
-        setSearch: (text) => dispatch(setSearch(text)),
-        setBookFound: (ArrayBOoks) => dispatch(setBookFound(ArrayBOoks))
-    }
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Main)
+      setSearch: text => dispatch(setSearch(text)),
+      setBookFound: ArrayBOoks => dispatch(setBookFound(ArrayBOoks))
+    };
+  }
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Main);
